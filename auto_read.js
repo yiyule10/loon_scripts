5fad52a9cf68018d04c54551cb39bc0f351e8ec6
yiyule10 <71067732+yiyule10@users.noreply.github.com>
2021-12-05 19:01:17 +0800

    Update auto_read.js

--- auto_read.js
+++ auto_read.js
@@ -2,7 +2,7 @@
 Loon:
 
 [Script]
-http-response ^https?://mp\.weixin\.qq\.com/s.+? tag=自动阅读, response-body=true, script-path=https://raw.githubusercontent.com/yiyule10/loon_scripts/main/auto_read.js
+http-response ^https?://mp\.weixin\.qq\.com/s.+? tag=自动阅读, requires-body=true, script-path=https://raw.githubusercontent.com/yiyule10/loon_scripts/main/auto_read.js
 
 [mitm]
 hostname = mp.weixin.qq.com
