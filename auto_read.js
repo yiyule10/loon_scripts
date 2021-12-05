3aeedbe4228edc6fec48c461596a28ce615bc141
yiyule10 <71067732+yiyule10@users.noreply.github.com>
2021-12-05 22:49:43 +0800

    Update auto_read.js

--- auto_read.js
+++ auto_read.js
@@ -11,7 +11,7 @@
  * QX似乎直接添加到重写就行
 Loon:
 [Script]
-http-response ^https?://mp\.weixin\.qq\.com/s.+? tag=自动阅读, requires-body=true, script-path=https://raw.githubusercontent.com/yiyule10/loon_scripts/main/auto_read.js
+http-response ^https?://mp\.weixin\.qq\.com/s.+? tag=自动阅读, requires-body=true, timeout=10, script-path=https://raw.githubusercontent.com/yiyule10/loon_scripts/main/auto_read.js
 
 [MITM]
 hostname = mp.weixin.qq.com
