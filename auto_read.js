ecabeb309fd63d185d8dd8cc85a0cad05609b8d5
yiyule10 <71067732+yiyule10@users.noreply.github.com>
2021-12-06 09:29:45 +0800

    改一下配置

--- auto_read.js
+++ auto_read.js
@@ -22,7 +22,7 @@ hostname = mp.weixin.qq.com
 
 Surge:
 [Script]
-自动阅读 = type=http-request,requires-body=1,pattern=^https?://mp\.weixin\.qq\.com/s.+?,script-path=https://raw.githubusercontent.com/yiyule10/loon_scripts/main/auto_read.js
+自动阅读 = type=http-response,requires-body=1,pattern=^https?://mp\.weixin\.qq\.com/s.+?,script-path=https://raw.githubusercontent.com/yiyule10/loon_scripts/main/auto_read.js
 
 [MITM]
 hostname = mp.weixin.qq.com
